298eb13ad85bb86af0731ba3c05549334b6d3671
Slusks <samslusky@gmail.com>
2020-05-27 20:07:29 -0400

    Update - Champion Matchups
    Exploring different ways of comparing top lane champion matchups

--- sqltesting.py
+++ sqltesting.py
@@ -1,8 +1,10 @@
 import sqlite3
 import pprint
+# For viewing DB: https://inloop.github.io/sqlite-viewer/
 conn = sqlite3.connect('LCS_2020SpringMatchData.sqlite')
 c = conn.cursor()
 
+#takes a tuple list of wins and losses and returns the wins and losses per champion
 def player_result_array(tupleList):
     wins ={}
     losses ={}
@@ -22,17 +24,25 @@ def player_result_array(tupleList):
 
     return {"wins":wins, "Losses":losses}
 
-topLanerTupleList = c.execute('SELECT DISTINCT player FROM LCS_2020SpringMatchData where position="top" ').fetchall()
-topLanerList = [item for t in topLanerTupleList for item in t] #https://www.geeksforgeeks.org/python-convert-list-of-tuples-into-list/
-#print("topLaners", topLanerList)
+#returns list showing the number of wins and losses per top lane player per champion
+def topLanerDetails():
+    topLanerTupleList = c.execute('SELECT DISTINCT player FROM LCS_2020SpringMatchData where position="top" ').fetchall()
+    topLanerList = [item for t in topLanerTupleList for item in t] #https://www.geeksforgeeks.org/python-convert-list-of-tuples-into-list/
+    #print("topLaners", topLanerList)
 
-topLanerStats = {}
-for player in topLanerList:
-    playerTuples = c.execute('SELECT champion, wins FROM LCS_2020SpringMatchData WHERE player=?', (player,)).fetchall()
+    topLanerStats = {}
+    for player in topLanerList:
+        playerTuples = c.execute('SELECT champion, wins FROM LCS_2020SpringMatchData WHERE player=?', (player,)).fetchall()
 
-    playerChampions = player_result_array(playerTuples)
-    print(player, playerChampions)
-    topLanerStats[player] = playerChampions
+        playerChampions = player_result_array(playerTuples)
+        print(player, playerChampions) # output looks like TopLaner {'wins': {'name': num, 'name': num}, 'Losses': {'name': num, 'name': num}}
+        topLanerStats[player] = playerChampions
+    return topLanerStats
 
+
+
+topLaneChampionTupleList = c.execute('SELECT gameid, champion, wins FROM LCS_2020SpringMatchData where position="top" ORDER BY gameid DESC').fetchall()
+for i in topLaneChampionTupleList:
+    print (i)
         
 
